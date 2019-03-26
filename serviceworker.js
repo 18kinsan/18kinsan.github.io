eef496db6db9d78464f29a4502f4b49ac96f7a72
18kinsan <fridayandita@mail.ugm.ac.id>
2019-03-26 13:50:59 +0700

    revisi lagi

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'latihan-pwa-cache-v1';
+var CACHE_NAME = 'kuis-2-cache-v1';
 
 var urlToCache = [
     '/',
@@ -13,95 +13,91 @@ var urlToCache = [
     '/js/main.js'
 ];
 
-//install service worker
-self.addEventListener('install', function(event){
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache){
-                console.log('service worker do install..',cache);
-                return cache.addAll(urlToCache);
-            })
-    );
-});
-
-//aktivasi cache
-self.addEventListener('activate',function(event){
-    event.waitUntil(
-        caches.keys().then(function(cacheName){
-            return Promise.all(
-                //delete cache jika da versi yang lebih baru
-                cacheName.filter(function(cacheName){
-                    return cacheName !== CACHE_NAME;
-                }).map(function(cacheName){
-                    return caches.delete(cacheName);
-                })
-            );
+      caches.open(CACHE_NAME).then(function(cache) {
+          console.log('service worker do install...');
+          return cache.addAll(urlsToCache);
         })
     );
-});
-
-//fetch cache
-self.addEventListener('fetch',function(event){
-    var request = event.request;
-    var url = new URL(request.url);
-
-    //memisahkan cache file degan cache data API
-    if(url.origin === location.origin){
+  });
+  
+  //activation cache
+  self.addEventListener('activate', function(event){
+     event.waitUntil(
+        caches.keys().then(function(cacheNames){
+           return Promise.all(
+              cacheNames.filter(function(cacheName){
+                 return cacheName !== CACHE_NAME;
+              }).map(function(cacheName){
+                 return caches.delete(cacheName);
+              })
+           )
+        })
+     )
+  });
+  
+  //fetch cache
+  self.addEventListener('fetch', function(event){
+     var request = event.request;
+     var url = new URL(request.url);
+  
+     //Memisahkan Cache File dengan Cache API
+     if (url.origin === location.origin) {
         event.respondWith(
-            caches.match(request).then(function(response){
-                return response || fetch(request);
-            })
+           caches.match(request).then(function(response){
+              return response || fetch(request);
+           })
         )
-    } else {
+     }else {
         event.respondWith(
-            caches.open('list-mahasiswa-cache-v1')
-            .then(function(cache){
-                return fetch(request).then(function(liveRequest){
-                    cache.put(request,liveRequest.clone());
-                    return liveRequest;
-                }).catch(function(){
-                    return caches.match(request)
-                    .then(function(response){
-                        if(response) return response;
-                        return caches.match('fallback.json');
-                    })
-                })
-            })
+           caches.open('kuis-2-cache-v1').then(function(cache){
+              return fetch(request).then(function(liveRequest){
+                 cache.put(request, liveRequest.clone());
+                 return liveRequest;
+              }).catch(function(){
+                 return caches.match(request).then(function(response){
+                    if (response) return response;
+                    return caches.match('./fallback.json');
+                 })
+              })
+           })
         )
-    }
-    // event.respondWith(
-    //     caches.match(event.request)
-    //     .then(function(response){
-    //         console.log(response);
-    //     if(response){
-    //         return response;
-    //     }
-    //         return fetch(event.request);
-    // })
-    // )
-});
-
-if(navigator.serviceWorker){
-    window.addEventListener('load', function(){
-        navigator.serviceWorker.register('/serviceworker.js').then(function(reg){
-            console.log('SW regis sukses dgn skop', reg.scope);
-        }, function(err){
-            console.log("SW regis failed", err);
-        });
+     }
+  
+     // event.respondWith(
+     //    caches.match(event.request).then(function(response){
+     //       console.log(response);
+     //       if (response) {
+     //          return response;
+     //       }
+     //       return fetch(event.request);
+     //    })
+     // )
+  });
+  
+  //Event Click Notification
+  self.addEventListener('notificationclick', function(e){
+     var notification = e.notification;
+     var primaryKey = notification.data.primaryKey;
+     var action = e.action;
+     console.log(primaryKey);
+  
+     if (action == 'close') {
+        notification.close();
+     }else {
+        clients.openWindow('http://18kinsan.github.io');
+        notification.close();
+     }
+  });
+  
+  //register serviceWorker
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
     });
-}
-
-self.addEventListener('notificationclick', function (e){
-    var notification = e.notification;
-    var primaryKey = notification.data.primaryKey;
-    var action = e.action;
-
-    console.log(primaryKey);
-
-    if(action=== 'close'){
-        notification.close();
-
-    }else{
-        clients.openWindow('http://google.com');
-        notification.close();
-    }
-});
+  }
