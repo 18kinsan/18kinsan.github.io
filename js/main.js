3d587caf6e66b100f406180af22415f1632281b5
18kinsan <fridayandita@mail.ugm.ac.id>
2019-03-26 13:11:24 +0700

    main js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,42 @@
-
-(function ($) {
-    "use strict";
-
-    /*==================================================================
-    [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
-                showValidate(this);
-            }
-            else {
-                $(this).parent().addClass('true-validate');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-           $(this).parent().removeClass('true-validate');
-        });
-    });
-
-     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf135;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
-            });
-        });
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).removeClass('alert-validate');
-        $(thisAlert).find('.btn-hide-validate').remove();
-    }
+$(document).ready(function(){
     
-    
-
-})(jQuery);
-
-if ('serviceWorker' in navigator){
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/serviceworker.js').then(
-            function (reg) {
-                // registerasi service worker berhasil
-                console.log('SW registration success, scope :',reg.scope);
-            }, function (err) {
-                // reg failed
-                console.log('SW registration faild : ', err);
+        //belajar notifikasi
+        Notification.requestPermission(function(status){
+            console.log('Notification permission status : ',status);
+        });
+        function displayNotification(){
+            if(Notification.permission === 'granted'){
+                navigator.serviceWorker.getRegistration()
+                    .then(function(reg){
+                        var options = {
+                            body : 'Ini body notifikasi',
+                            icon : 'images/pistel.jpg',
+                            vibrate : [100,50,200],
+                            data : {
+                                dateOfArrival : Date.now(),
+                                primaryKey : 1
+                            },
+                            actions : [
+                                {action : 'explosure', title: 'Kunjungi Situs'},
+                                {action : 'close', title: 'Tutup'}
+                            ]
+                        }
+                        reg.showNotification('Judul Notifikasi',options);
+                    })
             }
-        )
-    })
-}
+        }
+        $('#btn-notification').on('click', function(){
+            displayNotification();
+        })
+    });     
+    if('serviceWorker' in navigator){
+        window.addEventListener('load',function(){
+            navigator.serviceWorker.register('/serviceworker.js').then(function(reg){
+                console.log('SW regis sukses dengan skop',reg.scope)
+                }, 
+                    function(err){
+                    console.log('sw regis failed',err);
+            })
+        })
+    } 
+    
